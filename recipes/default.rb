636d5e23eb62a8beee57a402fd3b846ec56c9113
Avinash Kumar <avinashkumar3497@gmail.com>
2026-05-26 17:54:18 +0530

    reverted previous commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,5 @@
 # this file will be empty in the actual execution
 
 gcp_ops_agent_configuration 'setup' do
-  environment 'int'
-  platform 'cf65'
-  role 'author'
-  server node.name
   action :create
 end
